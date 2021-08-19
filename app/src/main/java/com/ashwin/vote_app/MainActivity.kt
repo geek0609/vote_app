ce11f0a7caf27ca16052c8c9ac2b466599c67cba
Ashwin.D.S <astroashwin@outlook.com>
2021-08-19 16:28:53 +0530

    VoteApp: Add wrong email text to inform user
    Also do these misc changes while we are at it
    
    - Rebrand to VoteApp
    - Remove toasts on button click as toasts get pretty annoying if button is clicked repeatedly
    - Center the toast
    - kill functionality of button once correct email is entered

--- app/src/main/java/com/ashwin/vote_app/MainActivity.kt
+++ app/src/main/java/com/ashwin/vote_app/MainActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button:Button = findViewById(R.id.get_started_button)
+
+        val text:TextView = findViewById(R.id.textView)
         button.setOnClickListener{
             val intent = Intent(this, login_page::class.java)
             startActivity(intent)
--- app/src/main/java/com/ashwin/vote_app/login_page.kt
+++ app/src/main/java/com/ashwin/vote_app/login_page.kt
@@ -1,12 +1,16 @@
 package com.ashwin.vote_app
 
 import android.content.Intent
+import android.opengl.Visibility
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
 class login_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,13 +20,26 @@ class login_page : AppCompatActivity() {
         val otpCheck:Button = findViewById(R.id.otp_check_button)
         val otp:EditText = findViewById(R.id.enter_otp)
         otaButton.setOnClickListener{
-            val intent:Intent = Intent(this, login_page::class.java)
-            Toast.makeText(applicationContext, getString(R.string.otp_success), Toast.LENGTH_SHORT).show()
-            otp.visibility = View.VISIBLE
-            otpCheck.visibility = View.VISIBLE
-
+            val emailEntered = findViewById<EditText>(R.id.editText).text
+            var EmailFormatOK: Boolean = false
+            val warning = findViewById<TextView>(R.id.incorrectPassword)
+            if (emailEntered.endsWith("@iswkoman.com")){
+                EmailFormatOK = true
+                otp.visibility = View.VISIBLE
+                otpCheck.visibility = View.VISIBLE
+                warning.visibility = View.INVISIBLE
+                otaButton.isEnabled = false
+                val otp_toast = Toast.makeText(applicationContext, "OTP Sent\nCheck your email" , Toast.LENGTH_SHORT)
+                otp_toast.setGravity(Gravity.CENTER, 0, 0)
+                otp_toast.show()
+            } else {
+                EmailFormatOK =  false
+                warning.visibility = View.VISIBLE
+            }
             otpCheck.setOnClickListener{
-                Toast.makeText(applicationContext, "OTP Verified" , Toast.LENGTH_SHORT).show()
+                val otp_toast = Toast.makeText(applicationContext, "OTP Verified" , Toast.LENGTH_SHORT)
+                otp_toast.setGravity(Gravity.CENTER, 0, 0)
+                otp_toast.show()
             }
         }
     }
