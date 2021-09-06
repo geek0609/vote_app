75f8fc92b094240f1ce2025d10de9301e20ff11d
Ashwin.D.S <astroashwin@outlook.com>
2021-09-06 20:39:05 +0530

    Vote App: Add firebase and move to AndroidX

--- app/src/main/java/com/ashwin/vote_app/MainActivity.kt
+++ app/src/main/java/com/ashwin/vote_app/MainActivity.kt
@@ -1,18 +1,32 @@
 package com.ashwin.vote_app
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAuth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button:Button = findViewById(R.id.get_started_button)
 
+        mAuth = FirebaseAuth.getInstance()
+
+        val CurUser = mAuth.currentUser
+
         val text:TextView = findViewById(R.id.textView)
+
+
         button.setOnClickListener{
             val intent = Intent(this, login_page::class.java)
             startActivity(intent)
--- app/src/main/java/com/ashwin/vote_app/login_page.kt
+++ app/src/main/java/com/ashwin/vote_app/login_page.kt
@@ -2,49 +2,114 @@ package com.ashwin.vote_app
 
 import android.content.Intent
 import android.opengl.Visibility
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import org.w3c.dom.Text
 
 class login_page : AppCompatActivity() {
+
+    private lateinit var mAuth : FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    companion object{
+        private const val RC_SIGN_IN = 120
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_page)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        mAuth = FirebaseAuth.getInstance()
+
+
+
         val otaButton:Button = findViewById(R.id.otp_button)
         val otpCheck:Button = findViewById(R.id.otp_check_button)
         val otp:EditText = findViewById(R.id.enter_otp)
         otaButton.setOnClickListener{
-            val emailEntered = findViewById<EditText>(R.id.editText).text
-            var EmailFormatOK: Boolean = false
-            val warning = findViewById<TextView>(R.id.incorrectPassword)
-            if (emailEntered.endsWith("@iswkoman.com")){
-                EmailFormatOK = true
-                otp.visibility = View.VISIBLE
-                otpCheck.visibility = View.VISIBLE
-                warning.visibility = View.INVISIBLE
-                otaButton.isEnabled = false
-                val otp_toast = Toast.makeText(applicationContext, "OTP Sent\nCheck your email" , Toast.LENGTH_SHORT)
-                otp_toast.setGravity(Gravity.CENTER, 0, 0)
-                otp_toast.show()
-            } else {
-                EmailFormatOK =  false
-                warning.visibility = View.VISIBLE
-            }
-            otpCheck.setOnClickListener{
-                val otp_toast = Toast.makeText(applicationContext, "OTP Verified" , Toast.LENGTH_SHORT)
-                otp_toast.setGravity(Gravity.CENTER, 0, 0)
-                otp_toast.show()
-            }
+            signIn()
+            val text_view = findViewById<TextView>(R.id.textView2)
+            text_view.text = "nigOK"
+
         }
     }
 
     override fun onBackPressed() {
         finish()
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val exception = task.exception
+            if (task.isSuccessful){
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    val account = task.getResult(ApiException::class.java)!!
+                    Log.d("SignInActivity", "firebaseAuthWithGoogle:" + account.id)
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } catch (e: ApiException) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w("SignInActivity", "Google sign in failed", e)
+                    val text_view = findViewById<TextView>(R.id.textView2)
+                    text_view.text = "Not: OK1"
+                }
+            }
+            else{
+                Log.w("SignInActivity",exception.toString())
+                val text_view = findViewById<TextView>(R.id.textView2)
+                text_view.text = exception.toString()
+            }
+        }
+    }
+
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("SignInActivity", "signInWithCredential:success")
+                    val user = mAuth.currentUser
+                    val text_view = findViewById<TextView>(R.id.textView2)
+                    if (user != null) {
+                        text_view.text = user.email.toString()
+                    }
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("SignInActivity", "signInWithCredential:failure", task.exception)
+                    val text_view = findViewById<TextView>(R.id.textView2)
+                    text_view.text = "Not: OK"
+                }
+            }
     }
 }
